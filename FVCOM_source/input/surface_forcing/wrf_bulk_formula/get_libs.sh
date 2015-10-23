c629cc6abb7839fc22944938b2548277a5f64ec1
Pierre Cazenave <pica@pml.ac.uk>
2015-10-23 17:11:39 +0100

    Add script to download the required libraries.

---
+++ FVCOM_source/input/surface_forcing/wrf_bulk_formula/get_libs.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+wget -c https://www.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8.15-patch1/src/hdf5-1.8.15-patch1.tar.gz
+wget -c ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-4.3.3.tar.gz
